7b2aad8a65ab75a210e462b3aebfca62de6ad11e
sunzhenyang <874530670@qq.com>
2022-11-01 16:58:19 +0800

    静态代码推送

---
+++ assets/index.html.3d24a3c8.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c,a as _}from"./app.5b67e1df.js";const a={},n=_("p",null,"\u672C\u6559\u7A0B\u5168\u9762\u4ECB\u7ECD JavaScript \u6838\u5FC3\u8BED\u6CD5\uFF0C\u8986\u76D6\u4E86 ES5 \u548C DOM \u89C4\u8303\u7684\u6240\u6709\u5185\u5BB9\u3002",-1),o=[n];function r(s,l){return t(),c("div",null,o)}var d=e(a,[["render",r],["__file","index.html.vue"]]);export{d as default};
